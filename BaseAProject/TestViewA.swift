c4a226b19e68d7b1cb5a9d2e43c65b5120af46cf
LukiWen <lukiwen19@126.com>
2018-03-08 11:01:03 +0800

    新增类

---
+++ BaseAProject/TestViewA.swift
@@ -0,0 +1,21 @@
+//
+//  TestViewA.swift
+//  BaseAProject
+//
+//  Created by chuxin on 2018/3/8.
+//  Copyright © 2018年 wenhelu. All rights reserved.
+//
+
+import UIKit
+
+public class TestViewA: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
